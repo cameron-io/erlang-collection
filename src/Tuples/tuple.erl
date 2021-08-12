ef548e4fe4fe1ce7c32bc99e834a706085c418d0
Cameron <camerongallichan@gmail.com>
2021-08-12 12:52:44 +0100

    Added tuple example

---
+++ src/Tuples/tuple.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author Cameron G.
+%%% @copyright (C) 2021
+%%% @doc
+%%%
+%%% @end
+%%% Created : 12. Aug 2021 12:30
+%%%-------------------------------------------------------------------
+-module(tuple).
+-author("Cameron Gallichan").
+
+%% API
+-export([create/0]).
+
+create() ->
+	{ok, FullName = {FirstName, LastName} } = io:read("What is your name? "),
+	FirstName,
+	LastName,
+	FullName.
